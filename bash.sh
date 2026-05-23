cdd436f16557f384df9ea14be4d76414e67f429f
root <root@ip-172-31-37-7.us-west-2.compute.internal>
2026-05-23 13:58:53 +0000

    Commit-done

--- bash.sh
+++ bash.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 yum install httpd -y
-echo "<h1> Hello I AM Linux Machine Running in AWS </h1>"> /var/www/html/index.html
+echo "<h1> Hello This a Modified Version Of the Linux Machine  </h1>"> /var/www/html/index.html
 service httpd start
 chkconfig httpd on
