ecebdff321b285ddb3786f03112320032f0d2db5
root <root@ip-172-31-37-7.us-west-2.compute.internal>
2026-05-23 14:02:54 +0000

    1

--- bash.sh
+++ bash.sh
@@ -1,5 +1,1 @@
-#!/bin/bash
-yum install httpd -y
-echo "<h1> Hello This a Modified Version Of the Linux Machine  </h1>"> /var/www/html/index.html
-service httpd start
-chkconfig httpd on
+This is a Bash Modified
